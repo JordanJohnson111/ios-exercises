6b599d85e838cd1f74630c8066d11bf7b27edaaa
jordanjohnson111 <jordanjohnson111@gmail.com>
2016-06-21 11:12:56 -0500

    Assignment 7 completed

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,18 +11,21 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
+    ++number;    
     /* WORK HERE */
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
+    NSInteger sum = number1 + number2;
     /* WORK HERE */
-    return number1;
+    return sum;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
+    NSInteger remainder = dividend % divisor;
     /* WORK HERE */
-    return 0;
+    return remainder;
 }
 
 @end
