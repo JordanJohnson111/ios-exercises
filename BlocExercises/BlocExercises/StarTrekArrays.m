7d90a705ae03bd1a083cd840228583ad193994b1
jordanjohnson111 <jordanjohnson111@gmail.com>
2016-06-24 11:40:17 -0500

    Assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,44 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
+    NSArray *starTrekArray = [characterString componentsSeparatedByString:@";"];
     /* WORK HERE */
-    return @[];
+    return starTrekArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
+    NSString *starTrekString = [characterArray componentsJoinedByString:@";"];
     /* WORK HERE */
-    return @"";
+    return starTrekString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
+    NSString *starTrekString = [characterArray componentsJoinedByString:@";"];
+    NSMutableArray *starTrekArray = [[starTrekString componentsSeparatedByString:@";"]mutableCopy];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [starTrekArray sortUsingDescriptors:@[sortDescriptor]];
+    
     /* WORK HERE */
-    return @[];
+    return starTrekArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
+    NSString *starTrekString = [characterArray componentsJoinedByString:@";"];
+    NSMutableArray *starTrekArray = [[starTrekString componentsSeparatedByString:@";"]mutableCopy];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [starTrekArray filterUsingPredicate:containsWorf];
+    
+    if (starTrekArray.count >= 1) {
+        BOOL worfExists = YES;
+        return worfExists;
+    } else {
+        BOOL worfExists = NO;
+        return worfExists;
+    }
+
+    /* NSString *worfString = [starTrekArray componentsJoinedByString:@";"];
+    BOOL worfExists = worfString != nil; */
     /* WORK HERE */
-    return NO;
 }
 
 @end
