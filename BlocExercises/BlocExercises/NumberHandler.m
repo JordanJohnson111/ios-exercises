2934c8f3210b899b16a9a2d3d47b832bf0b9e2f5
jordanjohnson111 <jordanjohnson111@gmail.com>
2016-07-05 12:07:44 -0500

    struggling with part 3

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,15 +11,17 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    NSNumber *twice = number * 2;
+    NSNumber *twice = [NSNumber numberWithInt:[number intValue] * 2];
+    
+    
     /* WORK HERE */
     return twice;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    NSMutableArray *numberArray;
-    for (number; number<otherNumber;number++) {
-        [numberArray addObject:number];
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    for (number; number <= otherNumber; number++) {
+        [numberArray addObject:[NSNumber numberWithInt:number]];
 
     }
     /* WORK HERE */
@@ -27,8 +29,19 @@
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
+    NSMutableArray *sortedArray = [@[arrayOfNumbers] mutableCopy];
+    NSSortDescriptor *descending = [[NSSortDescriptor alloc] initWithKey:nil ascending:NO];
+    [sortedArray sortUsingDescriptors:@[descending]];
+    NSNumber *lowest = [NSNumber numberWithInt:[[sortedArray firstObject] intValue]];
     /* WORK HERE */
-    return 0;
+    return [lowest intValue];
 }
 
 @end
+
+
+
+
+
+
+
